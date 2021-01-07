b18a304ea8e59620353bbcc0b04bb16ce3219e72
Gustavo Gomes <gustavo.gomesspi@gmail.com>
2021-01-07 19:03:35 -0300

    rotas autenticadas

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: 'dfa7e38e3ef13f95785e86b91451d67d',
+        expiresIn: '1d',
+    },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,6 +1,7 @@
 import { getRepository } from "typeorm";
 import { compare } from "bcryptjs";
 import { sign } from 'jsonwebtoken';
+import authConfig from '../config/auth';
 
 import User from '../models/User';
 
@@ -31,9 +32,11 @@ class AuthenticateUserService {
             throw new Error('Incorrect email/password combination.');
         }
 
-        const token = sign({}, 'dfa7e38e3ef13f95785e86b91451d67d', {
+        const { secret, expiresIn } = authConfig.jwt;
+
+        const token = sign({}, secret , {
             subject: user.id,
-            expiresIn: '1d',
+            expiresIn,
         });
 
         return {
